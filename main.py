911ca78ddf7b398ee5f7d2067332b7f8d00ae4b1
jihun <altmxjdms@naver.com>
2024-01-16 14:37:01 +0900

    day3_V07

--- main.py
+++ main.py
@@ -20,16 +20,16 @@
 #
 # print(subject['python'])
 
-number=int(input("input number:"))
+number=int(input("input number: "))
 
-cnt=0
+is_prime=True
 i=2
 while i<number:
     if number%i==0:
-        cnt+=1
+        is_prime=False
         break
     i+=1
-if cnt==0:
+if is_prime:
     print(f'{number} is prime number')
 else:
     print(f'{number} is not prime number')
