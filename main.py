67e7ba1deed012cea0bf1acf918f0e0bf1fe9db3
jihun <altmxjdms@naver.com>
2024-01-16 11:44:36 +0900

    day3_V05

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 subjects="python c++ database linux"
 subject=input("수강신청과목 입력:")
 
-if subjects.find(subject) != -1:
-    print(f"해당 과목이 존재하비다 위치는 {subjects.find(subject)}입니다")
-else:
+
+try:
+    print(f"해당 과목이 존재하비다 위치는 {subjects.index(subject)}입니다")
+except ValueError:
     print("해당과목은 존재하지 않습니다")
 
 
