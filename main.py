79a36b6ea04431bd54ef921b188b96829201e5c1
jihun <altmxjdms@naver.com>
2024-01-16 11:34:23 +0900

    day3_03

--- main.py
+++ main.py
@@ -1,15 +1,68 @@
 
 
-
-# university="Inha\nUniversity"
+# course="2024 KEB Bootcamp"
 #
-# print(university)
+# print(course)
+#
+# #list_course=course.split()
+#
+# list_course=course.split("B")
+#
+# print(list_course)
 
-nubmer1=input("First number:")
+# numbers=input("Firstnumber Secondnumber:").split()
+#
+# print(numbers[0]+numbers[1])
+#
+# print(int(numbers[0])+int(numbers[1]))
 
-nubmer2=input("Second number:")
 
-print(nubmer1+nubmer2)
+# subjects=["python","c++",'database']
+#
+# subjects_str="/".join(subjects)
+#
+# subjects_str2=",".join(subjects)
+#
+# print(subjects_str)
+#
+# print(subjects_str2)
+
+
+# course="KEB 2024 KEB  bootcamp KEB"
+#
+# print(course)
+#
+# course=course.replace('KEB','inha',2)
+#
+# print(course)
+#
+
+# world="   earth  "
+#
+# print(world)
+#
+# print(world.strip())
+#
+# print(world.rstrip())
+#
+# print(world.lstrip())
+
+
+# course=".??KEB!!! 2024 KEB?  bootcamp KEB....!??"
+# print(course)
+#
+# print(course.strip(".!?"))
+#
+
+
+a="asd ghj"
+
+for i in range(len(a)):
+    print(a[i])
+
+
+
+
 
 
 
