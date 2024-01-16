384a754d5b64bb871ba2e99384cf35df07f4331a
jihun <altmxjdms@naver.com>
2024-01-16 11:39:45 +0900

    day3_V04

--- main.py
+++ main.py
@@ -1,64 +1,10 @@
+subjects="python c++ database linux"
+subject=input("수강신청과목 입력:")
 
-
-# course="2024 KEB Bootcamp"
-#
-# print(course)
-#
-# #list_course=course.split()
-#
-# list_course=course.split("B")
-#
-# print(list_course)
-
-# numbers=input("Firstnumber Secondnumber:").split()
-#
-# print(numbers[0]+numbers[1])
-#
-# print(int(numbers[0])+int(numbers[1]))
-
-
-# subjects=["python","c++",'database']
-#
-# subjects_str="/".join(subjects)
-#
-# subjects_str2=",".join(subjects)
-#
-# print(subjects_str)
-#
-# print(subjects_str2)
-
-
-# course="KEB 2024 KEB  bootcamp KEB"
-#
-# print(course)
-#
-# course=course.replace('KEB','inha',2)
-#
-# print(course)
-#
-
-# world="   earth  "
-#
-# print(world)
-#
-# print(world.strip())
-#
-# print(world.rstrip())
-#
-# print(world.lstrip())
-
-
-# course=".??KEB!!! 2024 KEB?  bootcamp KEB....!??"
-# print(course)
-#
-# print(course.strip(".!?"))
-#
-
-
-a="asd ghj"
-
-for i in range(len(a)):
-    print(a[i])
+if subjects.find(subject) != -1:
+    print(f"해당 과목이 존재하비다 위치는 {subjects.find(subject)}입니다")
+else:
+    print("해당과목은 존재하지 않습니다")
 
 
 
