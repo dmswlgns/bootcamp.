580c756f4a2e90bd6881df6145e079cd31dd3c5e
jihun <altmxjdms@naver.com>
2024-01-16 14:10:56 +0900

    day3_V06

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-subjects="python c++ database linux"
-subject=input("수강신청과목 입력:")
+# subjects="python c++ database linux"
+# subject=input("수강신청과목 입력:")
+#
+#
+# try:
+#     print(f"해당 과목이 존재하비다 위치는 {subjects.index(subject)}입니다")
+# except ValueError:
+#     print("해당과목은 존재하지 않습니다")
+
+#print("%e"% 0.333)
 
 
-try:
-    print(f"해당 과목이 존재하비다 위치는 {subjects.index(subject)}입니다")
-except ValueError:
-    print("해당과목은 존재하지 않습니다")
+# subject={
+#     'python':'kim',
+#     'c++':'sung',
+#     'data structure':'kang'
+# }
+#
+# print("{0[python]} {0[c++]}".format(subject))
+#
+# print(subject['python'])
 
+number=int(input("input number:"))
 
-
-
-
+cnt=0
+i=2
+while i<number:
+    if number%i==0:
+        cnt+=1
+        break
+    i+=1
+if cnt==0:
+    print(f'{number} is prime number')
+else:
+    print(f'{number} is not prime number')
 
 
 
