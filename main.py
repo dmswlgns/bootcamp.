4f8c57324b4ce9550cd4645313e1aed5cc5727ca
jihun <altmxjdms@naver.com>
2024-01-16 15:01:50 +0900

    day3_V010

--- main.py
+++ main.py
@@ -1,60 +1,17 @@
-# subjects="python c++ database linux"
-# subject=input("수강신청과목 입력:")
-#
-#
-# try:
-#     print(f"해당 과목이 존재하비다 위치는 {subjects.index(subject)}입니다")
-# except ValueError:
-#     print("해당과목은 존재하지 않습니다")
 
-#print("%e"% 0.333)
+number=int(input("input number: "))
+is_prime=True
 
+if number<2:
+    print(f'{number} is not prime number')
+else:
+    for i in range(2,number):
+        if number%i==0:
+            is_prime=False
+            break
+    if is_prime:
+        print(f'{number} is prime number')
+    else:
+        print(f'{number} is not prime number')
 
-# subject={
-#     'python':'kim',
-#     'c++':'sung',
-#     'data structure':'kang'
-# }
-#
-# print("{0[python]} {0[c++]}".format(subject))
-#
-# print(subject['python'])
-
-
-
-# number=int(input("input number: "))
-# is_prime=True
-#
-# if number<2:
-#     print(f'{number} is not prime number')
-# else:
-#     i=2
-#     while i<number:
-#         if number%i==0:
-#             is_prime=False
-#             break
-#         i+=1
-#     if is_prime:
-#         print(f'{number} is prime number')
-#     else:
-#         print(f'{number} is not prime number')
-#
-
-
-univ="inha"
-
-i=0
-while i<len(univ):
-    print(univ[i],end='')
-    i+=1
-
-print()
-
-for letter in univ:
-    print(letter,end='')
-
-print()
-
-for i in range(len(univ)):
-    print(univ[i],end='')
 
