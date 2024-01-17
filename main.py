bb54a891ca59665f90ed63ea0d565c7be6320f75
jihun <altmxjdms@naver.com>
2024-01-17 15:58:03 +0900

    day4_v2

--- main.py
+++ main.py
@@ -1,54 +1,80 @@
-while True:
-    menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Prime1   4) Prime2   5) Quit program : ")
 
-    if menu == '1':
-        fahrenheit = float(input('Input Fahrenheit : '))
-        print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
-    elif menu == '2':
-        celsius = float(input('Input Celsius : '))
-        print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
-    elif menu == '3':           #성능의 문제 숫자가 클수록 for문을 많이 반복해야한다.
-        number = int(input("Input number : "))
-        is_prime = True
+# prime number
+# numbers = input("Input first second number : ").split()
+# n1 = int(numbers[0])
+# n2 = int(numbers[1])
+#
+# if n1 > n2:
+#     n1, n2 = n2, n1
+#
+# for number in range(n1, n2+1):
+#     is_prime = True
+#
+#     if number < 2:
+#         continue  #pass
+#     else:
+#         i = 2
+#         while i*i < number:  # performance issue
+#             if number % i == 0:
+#                 is_prime = False
+#                 break
+#             i = i + 1
+#             #print(i, end=' ')  # loop count
+#         if is_prime:
+#             pass
+#             print(number, end=' ')
 
-        if number < 2:
-            print(f'{number} is NOT prime number!')
-        else:
-            for i in range(2, number):
-                if number % i == 0:
-                    is_prime = False
-                    break
 
-            if is_prime:
-                print(f'{number} is prime number')
-            else:
-                print(f'{number} is NOT prime number!')
-    elif menu == '4':                           #입력값이 1개일떄 오류가 발생한다.
-        numbers = input("Input first second number : ").split()
-        n1 = int(numbers[0])
-        n2 = int(numbers[1])
+sugang=dict(python='kim',db='kang',cpp='sung')
 
-        if n1 > n2:
-            n1, n2 = n2, n1
 
-        for number in range(n1, n2 + 1):
-            is_prime = True
+# sugang['datastructure']='park'
+#
+# print(sugang)
+#
+# print(sugang['db'])
+#
+# print(sugang.get("db"))
+#
+# print(sugang.get("opensource","not exist"))
 
-            if number < 2:
-                # pass
-                continue
-            else:
-                for i in range(2, number):             #중복코드 반복을 제거할수 있다.
-                    if number % i == 0:
-                        is_prime = False
-                        break
-                if is_prime: print(number, end=' ')
-        print()
-    elif menu == '5':
-        print('Terminate Program.')
-        break
-    else:
-        print('Invalid Menu!')
+
+# for subject,prof in sugang.items():
+#     print(f'{subject}과목 담담교수는 {prof}입니다')
+#
+#
+# for k in sugang:
+#     print(k)
+#
+# for v in sugang.values():
+#     print(v)
+#
+# for s in sugang.items():
+#     print(s)
+import random
+
+
+drink_food={
+    "위스키":"초콜릿",
+    "와인":"치즈",
+    "소주":'삼겹살',
+    "고량주":'양꼬치'
+}
+drink_food_keys=list(drink_food)
+
+print(random.choice(drink_food_keys))
+
+print(drink_food_keys)
+
+drink=input(f'다음 술중에 고르세요 \n{drink_food_keys[0]},{drink_food_keys[1]},{drink_food_keys[2]},{drink_food_keys[3]},종료')
+
+print(drink_food[drink])
+
+
+
+
+
+
 
 
 
