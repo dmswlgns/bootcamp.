75f37736a58fc42affc1e0f5cbc99b12acdd3f45
jihun <altmxjdms@naver.com>
2024-01-17 18:30:35 +0900

    day4_assingment2

--- main.py
+++ main.py
@@ -1,58 +1,24 @@
 
+import random
+drinks_foods = {"위스키": "초콜릿", "와인": "치즈", "소주": "삽겹살", "고량주": "양꼬치"}
 
-#연습문제 8.1
+drinks=list(drinks_foods.keys())
+food=list(drinks_foods.values())
 
-# e2f=dict()
-# e2f['dog']='chien'
-# e2f['cat']='chat'
-# e2f['walrus']='morse'
-#
-# print(e2f)
-#
-# 연습문제 8.2
-# print(e2f['walrus'])
-#
-# 연습문제 8.3
-# f2e=dict((v,k) for k,v in e2f.items())
-#
-# print(f2e)
-#
-# 연습문제 8.4
-#
-# print(f2e['chien'])
-#
-# 연습문제 8.5
-# for k in e2f.keys():
-#     print(k,end=" ")
-
-#연습문제 8.6
-
-# life=dict()
-# spices=dict()
-#
-# spices['cats']='Henri'
-# life['animals']=spices
-#
-# spices['octopi']='Grumpy'
-# life['animals']=spices
-#
-# spices['emus']='Lucy'
-# life['animals']=spices
-#
-# life['plants']={}
-# life['ohter']={}
-#
-# print(life)
-
-#연습문제 8.7
-#print(life.keys())
-
-#연습문제 8.8
-#print(life['animals'].keys())
-
-#연습문제 8.9
-# print(life['animals']['cats'])
-#
-# squares={k:k*k for k in range(10)}
-#
-# print(squares)
+print(f'1){drinks[0]}, 2){drinks[1]}, 3){drinks[2]}, 4){drinks[3]}, 5)아무거나 6)종료')
+while True:
+    select=int(input("주류를 고르세요: "))
+    if select==1:
+        print(f"{drinks[0]}와 어울리는 안주는 {drinks_foods[drinks[0]]}입니다")
+    if select == 2:
+        print(f'{drinks[1]}와 어울리는 안주는 {food[1]}입니다')
+    if select == 3:
+        print(f'{drinks[2]}와 어울리는 안주는 {food[2]}입니다')
+    if select == 4:
+        print(f'{drinks[3]}와 어울리는 안주는 {food[3]}입니다')
+    if select == 5:
+        k = random.choice([0,1,2,3])
+        print(f'{drinks[k]}와 어울리는 안주는 {food[k]}입니다')
+    if select ==6:
+        print("다음에 또 오세요")
+        break
