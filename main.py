7ef9a9437de895d842aa15c239c9f52ecc957d85
jihun <altmxjdms@naver.com>
2024-01-17 16:38:53 +0900

    day4_ãassignment8

--- main.py
+++ main.py
@@ -1,86 +1,58 @@
 
-# prime number
-# numbers = input("Input first second number : ").split()
-# n1 = int(numbers[0])
-# n2 = int(numbers[1])
+
+#연습문제 8.1
+
+# e2f=dict()
+# e2f['dog']='chien'
+# e2f['cat']='chat'
+# e2f['walrus']='morse'
 #
-# if n1 > n2:
-#     n1, n2 = n2, n1
+# print(e2f)
 #
-# for number in range(n1, n2+1):
-#     is_prime = True
+# 연습문제 8.2
+# print(e2f['walrus'])
 #
-#     if number < 2:
-#         continue  #pass
-#     else:
-#         i = 2
-#         while i*i < number:  # performance issue
-#             if number % i == 0:
-#                 is_prime = False
-#                 break
-#             i = i + 1
-#             #print(i, end=' ')  # loop count
-#         if is_prime:
-#             pass
-#             print(number, end=' ')
-
-
-sugang=dict(python='kim',db='kang',cpp='sung')
-
-
-# sugang['datastructure']='park'
+# 연습문제 8.3
+# f2e=dict((v,k) for k,v in e2f.items())
 #
-# print(sugang)
+# print(f2e)
 #
-# print(sugang['db'])
+# 연습문제 8.4
 #
-# print(sugang.get("db"))
+# print(f2e['chien'])
 #
-# print(sugang.get("opensource","not exist"))
+# 연습문제 8.5
+# for k in e2f.keys():
+#     print(k,end=" ")
 
+#연습문제 8.6
 
-# for subject,prof in sugang.items():
-#     print(f'{subject}과목 담담교수는 {prof}입니다')
+# life=dict()
+# spices=dict()
 #
+# spices['cats']='Henri'
+# life['animals']=spices
 #
-# for k in sugang:
-#     print(k)
+# spices['octopi']='Grumpy'
+# life['animals']=spices
 #
-# for v in sugang.values():
-#     print(v)
+# spices['emus']='Lucy'
+# life['animals']=spices
 #
-# for s in sugang.items():
-#     print(s)
-import random
+# life['plants']={}
+# life['ohter']={}
+#
+# print(life)
 
+#연습문제 8.7
+#print(life.keys())
 
-drink_food={
-    "위스키":"초콜릿",
-    "와인":"치즈",
-    "소주":'삼겹살',
-    "고량주":'양꼬치'
-}
-drink_food_keys=list(drink_food)
+#연습문제 8.8
+#print(life['animals'].keys())
 
-print(random.choice(drink_food_keys))
-
-print(drink_food_keys)
-
-drink=input(f'다음 술중에 고르세요 \n{drink_food_keys[0]},{drink_food_keys[1]},{drink_food_keys[2]},{drink_food_keys[3]},종료')
-
-print(drink_food[drink])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#연습문제 8.9
+# print(life['animals']['cats'])
+#
+# squares={k:k*k for k in range(10)}
+#
+# print(squares)
